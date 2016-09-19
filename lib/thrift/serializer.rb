f079b22aaeb626c0d8b20f7411a7e54a2147e00f
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-19 10:14:59 +0200

    Initial commit

---
+++ lib/thrift/serializer.rb
@@ -0,0 +1,7 @@
+require "thrift/serializer/version"
+
+module Thrift
+  module Serializer
+    # Your code goes here...
+  end
+end
---
+++ lib/thrift/serializer/version.rb
@@ -0,0 +1,5 @@
+module Thrift
+  module Serializer
+    VERSION = "0.1.0"
+  end
+end
